dad485be876e90be76efe99b207a89c9287bfdd2
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-25 02:15:38 +0800

    Home Calendar Day Widget Created

--- skin_first/lib/presentation/home_page/home_page.dart
+++ skin_first/lib/presentation/home_page/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:skin_first/presentation/home_page/widgets/home_app_bar_widget.dart';
 import 'package:skin_first/presentation/home_page/widgets/home_bottom_nav_bar.dart';
+import 'package:skin_first/presentation/home_page/widgets/home_calendar_day_widget.dart';
 import 'package:skin_first/presentation/home_page/widgets/home_search_nav.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,7 +17,30 @@ class HomePage extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: const HomeAppBarWidget(),
       ),
-      body: const HomeSearchNav(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const HomeSearchNav(),
+            const SizedBox(height: 16),
+            Container(
+              color: const Color(0xffCAD6FF),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    HomeCalendarDayWidget(isTrue: true, day: '9'),
+                    HomeCalendarDayWidget(isTrue: false, day: '10'),
+                    HomeCalendarDayWidget(isTrue: false, day: '11'),
+                    HomeCalendarDayWidget(isTrue: true, day: '12'),
+                    HomeCalendarDayWidget(isTrue: true, day: '13'),
+                    HomeCalendarDayWidget(isTrue: false, day: '14'),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: const HomeBottomAppBar(),
     );
   }
---
+++ skin_first/lib/presentation/home_page/widgets/home_calendar_day_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeCalendarDayWidget extends StatelessWidget {
+  const HomeCalendarDayWidget({super.key, required this.isTrue, required this.day});
+  // TODO: Temporary Boolean variable name
+  final bool isTrue;
+  final String day;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isTrue) {
+      return Expanded(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(children: <Widget>[
+            Text(
+              '10',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.black),
+            ),
+            const Text('Tue'),
+          ]),
+        ),
+      );
+    } else {
+      return Expanded(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: const Color(0xff2260FF),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(children: <Widget>[
+            Text(
+              '9',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
+            ),
+            Text('Mon', style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.white)),
+          ]),
+        ),
+      );
+    }
+  }
+}
