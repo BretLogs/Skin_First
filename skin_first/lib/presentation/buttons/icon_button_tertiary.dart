1d0e3c54b28af6fd0a5d4f0a251aa9f2aba30575
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-25 20:42:09 +0800

    Icon Buton Tertiary Created

---
+++ skin_first/lib/presentation/buttons/icon_button_tertiary.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IconButtonTertiary extends StatelessWidget {
+  const IconButtonTertiary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
