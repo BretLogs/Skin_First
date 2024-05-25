f795a52f34105574f9d2ec819e30925a5dcba515
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-25 17:22:54 +0800

    Icon Button Secondary Created

---
+++ skin_first/lib/presentation/buttons/icon_button_secondary.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class IconButtonSecondary extends StatelessWidget {
+  const IconButtonSecondary({super.key, required this.onPressed, required this.icon});
+  final Function() onPressed;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      style: const ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(
+          Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+      splashColor: Theme.of(context).colorScheme.primary,
+      icon: Icon(icon),
+    );
+  }
+}
