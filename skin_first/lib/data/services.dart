046cad6b979197d74695a509f81c50e7532d3d35
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-26 02:51:59 +0800

    Services class Created

---
+++ skin_first/lib/data/services.dart
@@ -0,0 +1,29 @@
+class Services {
+  static final Set<Map<String, String>> services = <Map<String, String>>{
+    <String, String>{
+      'title': 'Periodentals',
+      'service':
+          'Tempor est ut eiusmod sint in fugiat qui et excepteur duis occaecat velit ad. Proident quis nostrud duis eu fugiat. Laborum sunt mollit nostrud ullamco excepteur Lorem nisi ullamco voluptate incididunt. Cupidatat eiusmod est tempor esse do sit occaecat ea pariatur exercitation proident.'
+    },
+    <String, String>{
+      'title': 'Prostodontics',
+      'service':
+          'Do magna sit enim ipsum duis incididunt eiusmod est est excepteur consequat eiusmod. Fugiat irure excepteur dolor anim Lorem sit et ex minim. Ut sint exercitation ut veniam pariatur veniam Lorem ea. Laboris culpa laborum duis sint amet sunt occaecat adipisicing veniam excepteur eiusmod laboris.'
+    },
+    <String, String>{
+      'title': 'Orthopedics',
+      'service':
+          'Labore anim aliquip nulla laborum est ipsum magna mollit do. Aute qui exercitation eu dolore culpa do ullamco id deserunt magna aliqua. Mollit amet Lorem cupidatat ea elit non aliquip deserunt irure.'
+    },
+    <String, String>{
+      'title': 'Implantology',
+      'service':
+          'Sint elit anim laborum laborum ex. Adipisicing ad tempor non aute cupidatat eiusmod ex. Irure ullamco ipsum irure consectetur aute nostrud tempor laboris eu. Labore voluptate eu sit enim id est ea. Consequat amet fugiat magna adipisicing laboris nisi mollit nostrud quis. Lorem magna elit nostrud aliquip velit eiusmod do.'
+    },
+    <String, String>{
+      'title': 'Endodontics',
+      'service':
+          'Eiusmod Lorem adipisicing ad aute proident aute. Dolor elit commodo sint voluptate aute ea aliqua. Duis aliqua culpa mollit anim qui aliqua esse proident culpa anim duis.'
+    },
+  };
+}
