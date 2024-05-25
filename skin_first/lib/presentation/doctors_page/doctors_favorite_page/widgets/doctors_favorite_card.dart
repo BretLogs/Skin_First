9e55a5093232555b39f8ec660399b8ba6ce92191
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-26 00:15:02 +0800

    Doctors Favorite Card Done

---
+++ skin_first/lib/presentation/doctors_page/doctors_favorite_page/widgets/doctors_favorite_card.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:skin_first/presentation/buttons/elevated_button_secondary.dart';
+import 'package:skin_first/presentation/buttons/icon_button_secondary.dart';
+import 'package:skin_first/presentation/doctors_page/doctors_info_page/doctors_info_page.dart';
+
+class DoctorsFavoriteCard extends StatelessWidget {
+  const DoctorsFavoriteCard({super.key, required this.imageUrl});
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          color: const Color.fromARGB(63, 143, 169, 255),
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.network(
+                      imageUrl,
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.check_circle,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              'Professional Doctor',
+                              style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                                    color: Theme.of(context).colorScheme.primary,
+                                  ),
+                            ),
+                          ],
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            color: Colors.white,
+                            child: Row(
+                              children: <Widget>[
+                                const Icon(
+                                  Icons.star,
+                                  color: Color(0xff2260FF),
+                                  size: 16,
+                                ),
+                                Text(
+                                  '5.5',
+                                  style: Theme.of(context).textTheme.titleSmall,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Container(
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Dr. Olivia Turner, M.D.',
+                                      style: Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: 20),
+                                    ),
+                                    Text(
+                                      'Endodontics',
+                                      style: Theme.of(context).textTheme.labelMedium,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Icon(
+                                Icons.favorite,
+                                size: 24,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                          alignment: Alignment.center,
+                          backgroundColor: MaterialStatePropertyAll<Color>(Color(0xff2260FF)),
+                          foregroundColor: MaterialStatePropertyAll(Colors.white),
+                        ),
+                        child: Text(
+                          'Make an Appointment',
+                          style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
