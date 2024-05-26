baf434098304338f779b0ea3a4081befd2bf0c63
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-26 16:00:09 +0800

    Profle Page Done and profile page selection option with boolean isLogout

--- skin_first/lib/presentation/profile_page/profile_page.dart
+++ skin_first/lib/presentation/profile_page/profile_page.dart
@@ -16,45 +16,50 @@ class ProfilePage extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: const Text('My Profile'),
       ),
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 20),
-          Center(
-            child: Stack(alignment: Alignment.bottomRight, children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.network(
-                  'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww',
-                  height: 150,
-                  width: 150,
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 20),
+            Center(
+              child: Stack(alignment: Alignment.bottomRight, children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(
+                    'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww',
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.edit_outlined,
-                  size: 20,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.white)),
-              )
-            ]),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'John Doe',
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Colors.black87,
-                ),
-          ),
-          const ProfilePageSelectionPrimary(icon: Icons.person, titleText: 'Profile'),
-          const ProfilePageSelectionPrimary(icon: Icons.favorite, titleText: 'Favorite'),
-          const ProfilePageSelectionPrimary(icon: Icons.wallet, titleText: 'Payment Method'),
-          const ProfilePageSelectionPrimary(icon: Icons.lock_outline, titleText: 'Privacy Policy'),
-          const ProfilePageSelectionPrimary(icon: Icons.settings, titleText: 'Settings'),
-          const ProfilePageSelectionPrimary(icon: CupertinoIcons.question, titleText: 'Help'),
-        ],
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.edit_outlined,
+                    size: 20,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.white)),
+                )
+              ]),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              'John Doe',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Colors.black87,
+                  ),
+            ),
+            const ProfilePageSelectionPrimary(icon: Icons.person, titleText: 'Profile', isLogout: false),
+            const ProfilePageSelectionPrimary(icon: Icons.favorite, titleText: 'Favorite', isLogout: false),
+            const ProfilePageSelectionPrimary(icon: Icons.wallet, titleText: 'Payment Method', isLogout: false),
+            const ProfilePageSelectionPrimary(icon: Icons.lock_outline, titleText: 'Privacy Policy', isLogout: false),
+            const ProfilePageSelectionPrimary(icon: Icons.settings, titleText: 'Settings', isLogout: false),
+            const ProfilePageSelectionPrimary(icon: CupertinoIcons.question, titleText: 'Help', isLogout: false),
+            const SizedBox(height: 40),
+            const ProfilePageSelectionPrimary(icon: Icons.logout, titleText: 'Logout', isLogout: true),
+            const ProfilePageSelectionPrimary(icon: Icons.catching_pokemon, titleText: 'Change User', isLogout: true),
+          ],
+        ),
       ),
       bottomNavigationBar: const HomeBottomAppBar(),
     );
--- skin_first/lib/presentation/profile_page/widgets/profile_page_selection_primary.dart
+++ skin_first/lib/presentation/profile_page/widgets/profile_page_selection_primary.dart
@@ -1,34 +1,59 @@
 import 'package:flutter/material.dart';
 
 class ProfilePageSelectionPrimary extends StatelessWidget {
-  const ProfilePageSelectionPrimary({super.key, required this.icon, required this.titleText});
+  const ProfilePageSelectionPrimary({super.key, required this.icon, required this.titleText, required this.isLogout});
   final IconData icon;
   final String titleText;
+  final bool isLogout;
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle buttonStyle;
+    if (!isLogout) {
+      buttonStyle = ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+            side: BorderSide(color: Theme.of(context).colorScheme.primary),
+          ),
+        ),
+      );
+    } else {
+      buttonStyle = ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+            side: BorderSide(color: Theme.of(context).colorScheme.primary),
+          ),
+        ),
+      );
+    }
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 4),
       child: TextButton(
+        style: buttonStyle,
         onPressed: () {},
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Icon(
-                  icon,
-                  size: 24,
-                ),
-                const SizedBox(width: 12),
-                Text(
-                  titleText,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(),
-                ),
-              ],
-            ),
-            const Icon(Icons.arrow_forward_ios_outlined),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    icon,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 12),
+                  Text(
+                    titleText,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(),
+                  ),
+                ],
+              ),
+              if (isLogout) Container() else const Icon(Icons.arrow_forward_ios_outlined),
+            ],
+          ),
         ),
       ),
     );
