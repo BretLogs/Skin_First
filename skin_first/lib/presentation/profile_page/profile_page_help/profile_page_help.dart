6f2fa31c633d9798492fbb05e51cef77379bd8c9
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-28 02:52:01 +0800

    profile page help search bar created

--- skin_first/lib/presentation/profile_page/profile_page_help/profile_page_help.dart
+++ skin_first/lib/presentation/profile_page/profile_page_help/profile_page_help.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProfilePageHelp extends StatelessWidget {
   const ProfilePageHelp({super.key});
@@ -7,14 +8,28 @@ class ProfilePageHelp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Help Center')),
-      body: const Column(
+      body: Column(
         children: <Widget>[
-          Center(
-            child: Column(
-              children: <Widget>[
-                Text('How can we help you?'),
-                SearchBar(),
-              ],
+          Container(
+            color: Theme.of(context).colorScheme.primary,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  Text(
+                    'How can we help you?',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 20),
+                    child: SearchBar(
+                      leading: Icon(Icons.search),
+                      padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 20)),
+                      hintText: 'Search...',
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         ],
