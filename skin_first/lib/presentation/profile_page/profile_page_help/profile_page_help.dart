e4b54f7bca3c044ec42951ccaf2d49421853e45e
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-28 03:24:30 +0800

    Profile Page Help Card Done

--- skin_first/lib/presentation/profile_page/profile_page_help/profile_page_help.dart
+++ skin_first/lib/presentation/profile_page/profile_page_help/profile_page_help.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:skin_first/presentation/buttons/elevated_button_secondary.dart';
+import 'package:skin_first/presentation/buttons/elevated_button_tertiary.dart';
+import 'package:skin_first/presentation/profile_page/profile_page_help/widgets/profile_page_help_card.dart';
 
 class ProfilePageHelp extends StatelessWidget {
   const ProfilePageHelp({super.key});
@@ -30,6 +34,44 @@ class ProfilePageHelp extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(child: ElevatedButtonSecondary(onPressed: () {}, textString: 'FAQ')),
+                const SizedBox(width: 12),
+                Expanded(child: ElevatedButtonTertiary(onPressed: () {}, textString: 'Contact Us'))
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ElevatedButtonSecondary(onPressed: () {}, textString: 'Popular Topic'),
+                  const SizedBox(width: 8),
+                  ElevatedButtonTertiary(onPressed: () {}, textString: 'General'),
+                  const SizedBox(width: 8),
+                  ElevatedButtonTertiary(onPressed: () {}, textString: 'Services')
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                ProfilePageHelpCard(
+                    faqName: 'In nisi amet id tempor',
+                    faqDescription:
+                        'Ut do elit consequat sunt labore et nisi dolore. Ullamco aliquip est aliqua velit non laboris non esse sint ullamco mollit laboris deserunt. Enim non quis eiusmod fugiat consequat ipsum sint et. Sit nulla commodo sint nisi exercitation. Minim dolore nostrud Lorem consectetur est aute aute.')
+              ],
             ),
           )
         ],
---
+++ skin_first/lib/presentation/profile_page/profile_page_help/widgets/profile_page_help_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ProfilePageHelpCard extends StatelessWidget {
+  const ProfilePageHelpCard({super.key, required this.faqName, required this.faqDescription});
+  final String faqName;
+  final String faqDescription;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.symmetric(horizontal: BorderSide(color: Theme.of(context).colorScheme.primary)),
+        ),
+        child: Theme(
+          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 20),
+            collapsedIconColor: Theme.of(context).colorScheme.primary,
+            iconColor: Theme.of(context).colorScheme.primary,
+            title: Text(
+              faqName,
+              style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            children: <Widget>[
+              Text(
+                faqDescription,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
