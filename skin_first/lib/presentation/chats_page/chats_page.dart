fbd784a37d5bbd19d7bd22bcf5694bb5ae52c215
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-28 08:35:12 +0800

    Chats Page chat Done

--- skin_first/lib/presentation/chats_page/chats_page.dart
+++ skin_first/lib/presentation/chats_page/chats_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:skin_first/data/dentist.dart';
+import 'package:skin_first/presentation/chats_page/widgets/chats_page_chat.dart';
+import 'package:skin_first/presentation/home_page/widgets/home_bottom_nav_bar.dart';
 
 class ChatsPage extends StatelessWidget {
   const ChatsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Messages'),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ChatsPageChat(),
+            ChatsPageChat(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const HomeBottomAppBar(),
+    );
   }
 }
---
+++ skin_first/lib/presentation/chats_page/widgets/chats_page_chat.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ChatsPageChat extends StatelessWidget {
+  const ChatsPageChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset(
+              'assets/images/profile_pic.jpg',
+              height: 80,
+              width: 80,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Israel Breta',
+                  style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Colors.black87),
+                ),
+                Text(
+                  'Est ad commodo labore enim cupidatat Lorem dolore reprehenderit nostrud labore eu do irure.',
+                  style: Theme.of(context).textTheme.bodySmall,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 24),
+          Text(
+            '2hrs',
+            style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Colors.black54),
+          ),
+        ],
+      ),
+    );
+  }
+}
